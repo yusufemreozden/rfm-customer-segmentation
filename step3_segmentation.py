a35256a99d5dad0c9ed8524f15374fd9b843c914
Yusuf Emre ÖZDEN <112346866+yusufemreozden@users.noreply.github.com>
2025-03-30 02:21:05 +0300

    Update step3_segmentation.py

--- step3_segmentation.py
+++ step3_segmentation.py
@@ -2,49 +2,48 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 
-# 1. Veriyi oku
 df = pd.read_csv("rfm_data.csv")
 
-# 2. Sadece sayısal RFM kolonlarını seç
+# sayısal RFM kolonlarını seçme
 rfm = df[["Recency", "Frequency", "Monetary"]]
 
-# 3. Standardize et
+# standardize
 scaler = StandardScaler()
 rfm_scaled = scaler.fit_transform(rfm)
 
-# 4. KMeans modelini oluştur (k=4)
+# KMeans model (k=4)
 kmeans = KMeans(n_clusters=4, random_state=42)
 df["Segment"] = kmeans.fit_predict(rfm_scaled)
 
-# 5. Segment ortalamalarını görselle
+# ortalamaların görseli
 print("\n📊 Segment Analizi:\n")
 print(df.groupby("Segment")[["Recency", "Frequency", "Monetary"]].mean().round(2))
 
-# 6. Segmentlenmiş veriyi kaydet
+# segmented datayı kaydet
 df.to_csv("rfm_segmented.csv", index=False)
 print("\n💾 Segmentlenmiş veri 'rfm_segmented.csv' olarak kaydedildi.")
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 
-# 1. Veriyi oku
+# read to data
 df = pd.read_csv("rfm_data.csv")
 
-# 2. Sadece sayısal RFM kolonlarını seç
+# sadece sayısal RFM kolonlarını seç
 rfm = df[["Recency", "Frequency", "Monetary"]]
 
-# 3. Standardize et
+# standardize et
 scaler = StandardScaler()
 rfm_scaled = scaler.fit_transform(rfm)
 
-# 4. KMeans modelini oluştur (k=4)
+# KMeans modelini oluştur (k=4)
 kmeans = KMeans(n_clusters=4, random_state=42)
 df["Segment"] = kmeans.fit_predict(rfm_scaled)
 
-# 5. Segment ortalamalarını görselle
+# segment ortalamalarını görselle
 print("\n📊 Segment Analizi:\n")
 print(df.groupby("Segment")[["Recency", "Frequency", "Monetary"]].mean().round(2))
 
-# 6. Segmentlenmiş veriyi kaydet
+# segmentlenmişleri kaydet
 df.to_csv("rfm_segmented.csv", index=False)
 print("\n💾 Segmentlenmiş veri 'rfm_segmented.csv' olarak kaydedildi.")
