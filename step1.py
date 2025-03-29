f3a95c3a11eee080a46cf133b1f64b8907c5f85d
Yusuf Emre ÖZDEN <112346866+yusufemreozden@users.noreply.github.com>
2025-03-30 02:18:20 +0300

    Update step1.py

--- step1.py
+++ step1.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
-# Veriyi oku
 df = pd.read_csv("rfm_data.csv")
 
-# Genel bilgiler
 print("✅ Veri seti boyutu:", df.shape)
 print("\n📌 İlk 5 Satır:\n", df.head())
 print("\n🔎 Veri Tipleri:\n", df.dtypes)
